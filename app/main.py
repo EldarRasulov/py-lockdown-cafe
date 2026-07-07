8d54824f976de706f61f0e507052345156fe7506
Eldar RasuLov <morddiff@gmail.com>
2026-07-07 10:03:52 +0400

    lockdown-cafe 0.3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from .errors import VaccineError, NotWearingMaskError
 
 
-def go_to_cafe(friends: list, cafe) -> str:
+def go_to_cafe(friends: list, cafe: str) -> str:
     masks_to_buy = 0
 
     for friend in friends:
